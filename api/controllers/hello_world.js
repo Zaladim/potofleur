f4256f2f8a0a5cb69d3d82834e00f5afea0ab34e
Paul <paul.lacaille@epitech.eu>
2020-02-11 11:22:11 +0100

    Initial commit

--- api/controllers/hello_world.js
+++ api/controllers/hello_world.js
@@ -38,7 +38,7 @@ function hello(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
   var name = req.swagger.params.name.value || 'stranger';
   var test = req.swagger.params.test.value;
-  var hello = util.format('Hello, %s!', name);
+  var hello = util.format('Kiwi, %s!', test);
   var hello2 = util.format('Hello, %s!', test);
 
   // See calls
